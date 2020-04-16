9edcc0ed848ba5b6e622f96c4308b241cf6aa4ff
Refky Wijaya <priv8refky@gmail.com>
2020-04-16 17:14:19 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+//profaucet data 
+$ua = "Mozilla/5.0 (Linux; U; Android 10; in-id; RMX1971 Build/QKQ1.190918.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 HeyTapBrowser/45.7.2.0.2beta";
+$cookie = "cookie: PHPSESSID=qs7gtmor0efcvth9c7nubej690; DOGEToken=DodGxx7IxnspaTCp24kTjOf34mEh6PzB";
+//getdoge data
+$ua1 = "User-Agent: Mozilla/5.0 (Linux; Android 10; RMX1971) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Mobile Safari/537.36";
+$cookie1 = "cookie: _ga=GA1.2.1197521082.1586455065";
+$cookie2 = "cookie: _gid=GA1.2.125020910.1586455065";
+$cookie3 = "cookie: _gat_gtag_UA_131115007_1=1";
+$tokendoge = "cookie: DOGEToken=CDRHJyKNZwwz5UU6EZMG2nYcbs1BYfDR";
+$phpid = "cookie: PHPSESSID=5ec0b77f6502d7b9f41164f53c4edad4";
+
+
+
+?>
